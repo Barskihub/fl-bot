91f3baff906d363c3f44f9f7b64d81ce3f10dfcc
Arthur Barski <barski.10877716@gmail.com>
2020-05-13 12:46:00 +0300

    add webhook

--- check-csv.py
+++ check-csv.py
@@ -2,6 +2,9 @@ import csv
 import subprocess
 import time
 import telebot
+import os
+from flask import Flask, request
+import logging
 
 bot = telebot.TeleBot('1141013801:AAEq32jIEQ0mjppuXm9t_XbldrXCDhVch3c')
 
@@ -34,22 +37,39 @@ class CheckBase:
         self.message = ""
 
 
-if __name__ == "__main__":
-    base = CheckBase("fl-python.csv")
-    bashCommand = "scrapy runspider fl/spiders/fl_title.py"
+base = CheckBase("fl-python.csv")
+bashCommand = "scrapy runspider fl/spiders/fl_title.py"
 
-    @bot.message_handler(commands=['start'])
-    def start_message(message):
-        i = 0
-        while i < 60000:
-            subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-            base.open()
-            base.check()
-            if base.message != "":
-                bot.send_message(message.chat.id, base.message)
-            base.rewrite()
-            time.sleep(60)
-            i += 1
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    while True:
+        subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        base.open()
+        base.check()
+        if base.message != "":
+            bot.send_message(message.chat.id, base.message)
+        base.rewrite()
+        time.sleep(60)
 
 
-    bot.polling()
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://serene-peak-20980.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
