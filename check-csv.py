2d599c7db4266c4c34b50da219e1f72938cf50bd
Arthur Barski <barski.10877716@gmail.com>
2020-05-13 13:02:56 +0300

    fixv3

--- check-csv.py
+++ check-csv.py
@@ -65,7 +65,7 @@ if "HEROKU" in list(os.environ.keys()):
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="https://serene-peak-20980.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
+        bot.set_webhook(url="https://serene-peak-20980.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
         return "?", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
